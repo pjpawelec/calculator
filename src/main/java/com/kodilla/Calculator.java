48178433d7a0a716fd1abc8b5a33cbb476e54a48
pjpawelec <pj.pawelec@gmail.com>
2020-02-17 20:20:14 +0100

    4.6 commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,17 +1,19 @@
 package com.kodilla;
 
 class Calculator {
-    public void addNumbers(int a, int b) {
-        System.out.println("Wynik dodawania: " + (a + b));
+    public int addNumbers(int a, int b) {
+        return a + b;
     }
 
-    public void subtractNumbers(int a, int b) {
-        System.out.println("Wynik odejmowania: " + (a - b));
+    public int subtractNumbers(int a, int b) {
+        return a - b;
     }
 
     public static void main(String args[]) {
         Calculator run = new Calculator();
-        run.addNumbers(12, 23);
-        run.subtractNumbers(44, 23);
+        int resultOfAddition = run.addNumbers(12, 23);
+        int resultOfSubtraction = run.subtractNumbers(44, 23);
+        System.out.println("Wynik dodawania: " + resultOfAddition);
+        System.out.println("Wynik odejmowania: " + resultOfSubtraction);
     }
 }
